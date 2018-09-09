9d86ec39c81cedf6f554fe19a2b71f390e9f640c
Falon Darville <falondarville@gmail.com>
2018-09-08 20:55:03 -0700

    set up react app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/home';
+
+class App extends Component {
+  render() {
+    return (
+
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/' component={home} />
+          </Switch>
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
